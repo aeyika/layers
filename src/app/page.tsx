fcf141399f892ae8d1e99ed143ad448567273409
aeyika <davidjunbowan@gmail.com>
2025-04-27 09:41:49 +0100

    Added integrations sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import CallToAction from "@/sections/CallToAction";
+import Faqs from "@/sections/Faqs";
 import Features from "@/sections/Features";
+import Footer from "@/sections/Footer";
 import Hero from "@/sections/Hero";
+import Integrations from "@/sections/Integrations";
 import Introduction from "@/sections/Introduction";
 import LogoTicker from "@/sections/LogoTicker";
 import Navbar from "@/sections/Navbar";
@@ -12,6 +16,10 @@ export default function Home() {
     <LogoTicker />
     <Introduction />
     <Features />
+    <Integrations />
+    <Faqs />
+    <CallToAction />
+    <Footer />
   </>
   );
 }
