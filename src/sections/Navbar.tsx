a9032832d2ab343b77eada756500d7366ce2ae56
aeyika <davidjunbowan@gmail.com>
2025-05-04 00:52:46 +0100

    Added motion to the dropdown navbar menu

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -5,9 +5,10 @@ import logoImage from "@/assets/images/logo.svg";
 import Button from "@/components/Button";
 import { useState } from "react";
 import { twMerge } from "tailwind-merge";
+import { AnimatePresence, motion } from "motion/react";
 
 const navLinks = [
-    { label: "Home", href: "/" },
+    { label: "Home", href: "#" },
     { label: "Features", href: "#features" },
     { label: "Integrations", href: "#integrations" },
     { label: "FAQs", href: "#faqs" },
@@ -16,9 +17,11 @@ const navLinks = [
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
     return (
-        <section className="py-4 lg:py-8 sticky top-0 z-50">
+        <>
+        <section className="py-4 lg:py-8 fixed w-full top-0 z-50">
             <div className="container max-w-5xl">
-                <div className="grid grid-cols-2 lg:grid-cols-3 bg-neutral-950/70 backdrop-blur border border-white/15 rounded-full p-2 items-center px-4 md:pr-2">
+                <div className="border border-white/15 rounded-[27px] md:rounded-full bg-neutral-950/70 backdrop-blur ">
+                    <div className="grid grid-cols-2 lg:grid-cols-3 p-2 items-center px-4 md:pr-2">
                     <div>
                         <Image src={logoImage} alt="Layers Logo" className="h-9 md:h-auto w-auto cursor-pointer" />
                     </div>
@@ -50,7 +53,7 @@ export default function Navbar() {
                                 y2="12"
                                 className={twMerge(
                                     "origin-left transition-all duration-200",
-                                    isOpen ? "rotate-45 -translate-y-2" : ""
+                                    isOpen ? "rotate-45 -translate-y-[8.35px]" : ""
                                 )}
                             />
                             <line
@@ -81,8 +84,32 @@ export default function Navbar() {
                             Sign Up
                         </Button>
                     </div>
+                    </div>
+                    <AnimatePresence>
+                        {isOpen && (
+                        <motion.div
+                            initial={{ height: 0 }}
+                            animate={{ height: "auto "}}
+                            exit={{ height: 0 }}
+                         className="overflow-hidden">
+                        <div className="flex flex-col items-center gap-4 py-4 ">
+                            {navLinks.map(link => (
+                                <a href={link.href} key={link.label} className="">
+                                    {link.label}
+                                </a>
+                            ))}
+                            <Button variant="secondary">Log In</Button>
+                            <Button variant="primary">Sign Up</Button>
+                        </div>
+                        </motion.div>
+                    )}
+                </AnimatePresence>
                 </div>
             </div>
         </section>
+        <div className="pb-[86px] md:pb-[98px] lg:px-[130px]">
+
+        </div>
+        </>
     );
 }
