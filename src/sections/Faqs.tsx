123763868a74b8f948423399a9cc2e50047fe8b8
aeyika <davidjunbowan@gmail.com>
2025-05-05 17:27:37 +0100

    (feat) Added animations for Faqs.tsx

--- src/sections/Faqs.tsx
+++ src/sections/Faqs.tsx
@@ -1,6 +1,8 @@
+"use client"
 import Tag from "@/components/Tag";
+import { useState } from "react";
 import { twMerge } from "tailwind-merge";
-
+import { AnimatePresence, motion } from "motion/react";
 const faqs = [
     {
         question: "How is Layers different from other design tools?",
@@ -25,7 +27,7 @@ const faqs = [
 ];
 
 export default function Faqs() {
-    const selectedIndex = 0;
+    const [selectedIndex, setSelectedIndex] =  useState(0);
     return(
         <section className="py-24" id="faqs">
             <div className="container">
@@ -33,19 +35,56 @@ export default function Faqs() {
                     <Tag>Faqs</Tag>
                 </div>
                 <h2 className="text-6xl font-medium mt-6 text-center max-w-xl mx-auto">Questions? We&apos;ve got <span className="text-lime-400">answers</span></h2>
-                <div className="mt-12 flex flex-col gap-6">
-                    {faqs.map((faq, faqIndex) =>
-                        <div key={faq.question} className="bg-neutral-900 rounded-2xl border border-white/10 p-6">
-                            <div className="flex justify-between items-center">
-                                <h3 className="font-medium">{faq.question}</h3>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={twMerge("feather feather-plus flex-shrink-0 text-lime-400", selectedIndex === faqIndex && "rotate-45")}><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
-
-                            </div>
-                            <div className={twMerge("mt-6", selectedIndex !== faqIndex && "hidden")}>
-                            <p className="text-white/50">{faq.answer}</p>
-                            </div>
+                <div className="mt-12 flex flex-col gap-6 cursor-pointer">
+                {faqs.map((faq, faqIndex) => (
+                    <div
+                        key={faq.question}
+                        className="bg-neutral-900 rounded-2xl border border-white/10 p-6"
+                        onClick={() => setSelectedIndex(faqIndex)}
+                    >
+                        <div className="flex justify-between items-center">
+                            <h3 className="font-medium">{faq.question}</h3>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                className={twMerge(
+                                    "feather feather-plus flex-shrink-0 text-lime-400 transition duration-500",
+                                    selectedIndex === faqIndex && "rotate-45"
+                                )}
+                            >
+                                <line x1="12" y1="5" x2="12" y2="19" />
+                                <line x1="5" y1="12" x2="19" y2="12" />
+                            </svg>
                         </div>
-                    )}
+                        <AnimatePresence>
+                        {selectedIndex === faqIndex && (
+                            <motion.div 
+                                initial={{
+                                    height: 0,
+                                    marginTop: 0,
+                                }}
+                                animate={{
+                                    height: "auto",
+                                    marginTop: 24,
+                                }}
+                                exit={{
+                                    height: 0,
+                                    marginTop: 0,
+                                }}
+                            className={twMerge("overflow-hidden")}>
+                                <p className="text-white/50">{faq.answer}</p>
+                            </motion.div>
+                        )}
+                        </AnimatePresence>
+                    </div>
+                ))}
                 </div>
             </div>
         </section>
