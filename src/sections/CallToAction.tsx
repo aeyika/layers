b033441492e83ff6000347b27d276e743e4eb7f4
aeyika <davidjunbowan@gmail.com>
2025-05-26 17:47:30 +0100

    (feat) Added animations for CallToAction.tsx

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,15 +1,26 @@
+"use client";
+import { motion } from "framer-motion";
+
 export default function CallToAction() {
     return (
-        <section className="py-24">
+        <section className="py-24 bg-black text-white">
             <div className="overflow-x-clip p-4 flex">
-            <div className="flex flex-none gap-16 text-7xl md:text-8xl font-medium">
-            {Array.from({ length: 10 }).map((_, i) => (
-                <div key={i} className="flex items-center gap-16">
-                    <span className="text-lime-400 text-7xl">&#10038;</span>
-                    <span>Try it for free</span>
-                </div>
-            ))}
-            </div>
+                <motion.div
+                    className="flex flex-none gap-16 text-7xl md:text-8xl font-medium whitespace-nowrap"
+                    animate={{ x: ["0%", "-50%"] }}
+                    transition={{
+                        repeat: Infinity,
+                        ease: "linear",
+                        duration: 20,
+                    }}
+                >
+                    {Array.from({ length: 10 }).map((_, i) => (
+                        <div key={i} className="flex items-center gap-16">
+                            <span className="text-lime-400 text-7xl">&#10038;</span>
+                            <span>Try it for free</span>
+                        </div>
+                    ))}
+                </motion.div>
             </div>
         </section>
     );
